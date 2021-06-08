2a76823e08647f9bffa19db02db5c87354e7fe21
llleyelll <chida.fua@gmail.com>
2021-06-08 16:05:40 +0700

    [#4] Add blur effect and overlay gradient background

--- SurveyApp/Modules/Login/LoginViewController.swift
+++ SurveyApp/Modules/Login/LoginViewController.swift
@@ -4,34 +4,36 @@
 //
 //  Created by Chananchida F. on 6/7/21.
 //
-//
 
 import UIKit
 import SnapKit
 
 // sourcery: AutoMockable
 protocol LoginViewInput: AnyObject {
-
+    
     func configure()
 }
 
 // sourcery: AutoMockable
 protocol LoginViewOutput: AnyObject {
-
+    
     func viewDidLoad()
 }
 
 final class LoginViewController: UIViewController {
-
+    
     var output: LoginViewOutput?
     
+    private let overlayView = UIView()
+    private let blurEffectView = UIVisualEffectView()
+    private let gradientLayer = CAGradientLayer()
     private let backgroundImageView = UIImageView()
     private let logoImageView = UIImageView()
     private let emailField = UITextField()
     private let passwordField = UITextField()
     private let loginButton = UIButton(type: .system)
     private let forgotButton = UIButton(type: .system)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         output?.viewDidLoad()
@@ -41,54 +43,60 @@ final class LoginViewController: UIViewController {
 // MARK: - LoginViewInput
 
 extension LoginViewController: LoginViewInput {
-
+    
     func configure() {
         setUpLayout()
         setUpViews()
     }
 }
 
+// MARK: - Private functions
+
 extension LoginViewController {
     
     private func setUpLayout() {
+        overlayView.layer.insertSublayer(gradientLayer, at: 0)
+
         view.addSubview(backgroundImageView)
+        view.addSubview(blurEffectView)
+        view.addSubview(overlayView)
         view.addSubview(logoImageView)
         view.addSubview(emailField)
         view.addSubview(passwordField)
         view.addSubview(forgotButton)
         view.addSubview(loginButton)
-        
+
         backgroundImageView.snp.makeConstraints {
             $0.edges.equalToSuperview()
-            $0.width.height.equalTo(UIScreen.main.bounds.width)
+            $0.width.equalTo(UIScreen.main.bounds.width)
         }
-        
+
         logoImageView.snp.makeConstraints {
             $0.top.equalToSuperview().offset(150.0)
             $0.centerX.equalToSuperview()
             $0.height.equalTo(40.0)
             $0.width.equalTo(180.0)
         }
-        
+
         emailField.snp.makeConstraints {
             $0.top.equalTo(logoImageView.snp.bottom).offset(120.0)
             $0.leading.trailing.equalToSuperview().inset(24.0)
             $0.height.equalTo(55.0)
         }
-        
+
         passwordField.snp.makeConstraints {
             $0.top.equalTo(emailField.snp.bottom).offset(20.0)
             $0.leading.trailing.equalToSuperview().inset(24.0)
             $0.height.equalTo(55.0)
         }
-        
+
         forgotButton.snp.makeConstraints {
             $0.top.equalTo(emailField.snp.bottom).offset(20.0)
             $0.trailing.equalTo(passwordField.snp.trailing)
             $0.leading.equalToSuperview().inset(280.0)
             $0.height.equalTo(55.0)
         }
-        
+
         loginButton.snp.makeConstraints {
             $0.top.equalTo(passwordField.snp.bottom).offset(20.0)
             $0.leading.trailing.equalToSuperview().inset(24.0)
@@ -101,12 +109,28 @@ extension LoginViewController {
         let logoImage = R.image.logo()
 
         navigationController?.isNavigationBarHidden = true
-        
+
         backgroundImageView.image = backgroundImage
-        
+
         logoImageView.image = logoImage
         logoImageView.contentMode = .scaleAspectFit
+
+        forgotButton.backgroundColor = .clear
+        forgotButton.setTitle("Forgot?", for: .normal)
+        forgotButton.titleLabel?.font = .regular(ofSize: .body)
+        forgotButton.tintColor = UIColor.white.withAlphaComponent(0.3)
+
+        loginButton.backgroundColor = .white
+        loginButton.setTitle("Log in", for: .normal)
+        loginButton.titleLabel?.font = .bold(ofSize: .body)
+        loginButton.tintColor = .black
+        loginButton.layer.cornerRadius = 10.0
         
+        setUpTextField()
+        setUpBlurOverlayView()
+    }
+    
+    private func setUpTextField() {
         emailField.placeholder = "Email"
         emailField.setPlaceholderColor(color: UIColor.white.withAlphaComponent(0.3))
         emailField.layer.cornerRadius = 12.0
@@ -117,7 +141,7 @@ extension LoginViewController {
         emailField.autocorrectionType = .no
         emailField.autocapitalizationType = .none
         emailField.font = .regular(ofSize: .body)
-        
+
         passwordField.placeholder = "Password"
         passwordField.setPlaceholderColor(color: UIColor.white.withAlphaComponent(0.3))
         passwordField.layer.cornerRadius = 12.0
@@ -126,16 +150,20 @@ extension LoginViewController {
         passwordField.backgroundColor = UIColor.white.withAlphaComponent(0.18)
         passwordField.isSecureTextEntry = true
         passwordField.font = .regular(ofSize: .body)
+    }
+    
+    private func setUpBlurOverlayView() {
+        let blurEffect = UIBlurEffect(style: .systemUltraThinMaterialDark)
+        blurEffectView.effect = blurEffect
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
-        forgotButton.backgroundColor = .clear
-        forgotButton.setTitle("Forgot?", for: .normal)
-        forgotButton.titleLabel?.font = .regular(ofSize: .body)
-        forgotButton.tintColor = UIColor.white.withAlphaComponent(0.3)
+        overlayView.frame = view.bounds
         
-        loginButton.backgroundColor = .white
-        loginButton.setTitle("Log in", for: .normal)
-        loginButton.titleLabel?.font = .bold(ofSize: .body)
-        loginButton.tintColor = .black
-        loginButton.layer.cornerRadius = 10.0
+        gradientLayer.frame = overlayView.bounds
+        gradientLayer.colors = [UIColor.black.withAlphaComponent(0.2).cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.25)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.75)
     }
 }
