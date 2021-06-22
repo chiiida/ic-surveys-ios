e6b9942f28c63d524bbed4adab1cee43593a4061
llleyelll <chida.fua@gmail.com>
2021-06-22 22:36:52 +0700

    [#45] Update test description

--- SurveyAppTests/Tests/Services/Network/API/UserSessionSpec.swift
+++ SurveyAppTests/Tests/Services/Network/API/UserSessionSpec.swift
@@ -32,11 +32,11 @@ class UserSessionSpec: QuickSpec {
             }
             
             context("when setting user credentials") {
-                it("should set the access token in UserSession") {
+                it("sets the access token in UserSession") {
                     expect(userSession.userCredential?.accessToken) == sampleUserCredential.accessToken
                 }
                 
-                it("should set the refresh token in UserSession") {
+                it("sets the refresh token in UserSession") {
                     expect(userSession.userCredential?.refreshToken) == sampleUserCredential.refreshToken
                 }
                 
@@ -50,7 +50,7 @@ class UserSessionSpec: QuickSpec {
                     UserSessionProvider.shared.clearCredentials()
                 }
                 
-                it("should delete credentials in UserSession") {
+                it("deletes credentials in UserSession") {
                     expect(userSession.userCredential?.accessToken).to(beNil())
                 }
                 
