db85129a561ae211aa7ac045e7acbb743ac87ada
llleyelll <chida.fua@gmail.com>
2021-06-22 16:06:30 +0700

    [#46] Update test description

--- SurveyAppTests/Tests/Services/Network/Services/AuthenticationServiceSpec.swift
+++ SurveyAppTests/Tests/Services/Network/Services/AuthenticationServiceSpec.swift
@@ -20,7 +20,7 @@ final class AuthenticationServiceSpec: QuickSpec {
         var userSession: UserSessionProtocolMock!
         var api: BaseAPI!
 
-        describe("AuthenticationService") {
+        describe("an AuthenticationService") {
 
             afterEach {
                 HTTPStubs.removeAllStubs()
@@ -34,7 +34,7 @@ final class AuthenticationServiceSpec: QuickSpec {
             }
 
             // MARK: – authenticateEmail
-            context("when call authenticateEmail") {
+            describe("when calling authenticateEmail") {
 
                 let grantType = "password"
                 let email = "email@example.com"
@@ -50,46 +50,50 @@ final class AuthenticationServiceSpec: QuickSpec {
                     "client_secret": clientSecret
                 ]
 
-                it("and get success response should receive response object correctly") {
-                    let data = JSON.AuthenticationService.authenticateEmailSuccess
+                context("the request returns success") {
+                    it("receives response object correctly") {
+                        let data = JSON.AuthenticationService.authenticateEmailSuccess
 
-                    stub(condition: pathEndsWith("/api/v1/oauth/token") && hasJsonBody(successRequestBody)) { _ in
-                        HTTPStubsResponse(data: data, statusCode: 200, headers: nil)
-                    }
-
-                    waitUntil(timeout: .seconds(10), action: { done in
-                        _ = authenticationService.authenticateEmail(email: email, password: password) { result in
-                            switch result {
-                            case .success(let response):
-                                expect(response.session?.userCredential?.accessToken) == "lbxD2K2BjbYtNzz8xjvh2FvSKx838KBCf79q773kq2c"
-                                expect(response.session?.userCredential?.refreshToken) == "3zJz2oW0njxlj_I3ghyUBF7ZfdQKYXd2n0ODlMkAjHc"
-                            case .failure:
-                                fail("result should be successful")
-                            }
-                            done()
+                        stub(condition: pathEndsWith("/api/v1/oauth/token") && hasJsonBody(successRequestBody)) { _ in
+                            HTTPStubsResponse(data: data, statusCode: 200, headers: nil)
                         }
-                    })
+
+                        waitUntil(timeout: .seconds(10), action: { done in
+                            _ = authenticationService.authenticateEmail(email: email, password: password) { result in
+                                switch result {
+                                case .success(let response):
+                                    expect(response.session?.userCredential?.accessToken) == "lbxD2K2BjbYtNzz8xjvh2FvSKx838KBCf79q773kq2c"
+                                    expect(response.session?.userCredential?.refreshToken) == "3zJz2oW0njxlj_I3ghyUBF7ZfdQKYXd2n0ODlMkAjHc"
+                                case .failure:
+                                    fail("result should be successful")
+                                }
+                                done()
+                            }
+                        })
+                    }
                 }
 
-                it("and get failure response should receive error response object correctly") {
-                    let data = JSON.AuthenticationService.authenticateEmailFailure
+                context("the request returns failure") {
+                    it("receives error response object correctly") {
+                        let data = JSON.AuthenticationService.authenticateEmailFailure
 
-                    stub(condition: pathEndsWith("/api/v1/oauth/token") && hasJsonBody(successRequestBody)) { _ in
-                        HTTPStubsResponse(data: data, statusCode: 403, headers: nil)
-                    }
-
-                    waitUntil(timeout: .seconds(10), action: { done in
-                        _ = authenticationService.authenticateEmail(email: email, password: password) { result in
-                            switch result {
-                            case .success:
-                                fail("result should be failure")
-                            case .failure(let error):
-                                expect(error.errors?[0].code) == "invalid_client"
-                                expect(error.errors?[0].detail) == "Client authentication failed due to unknown client, no client authentication included, or unsupported authentication method."
-                            }
-                            done()
+                        stub(condition: pathEndsWith("/api/v1/oauth/token") && hasJsonBody(successRequestBody)) { _ in
+                            HTTPStubsResponse(data: data, statusCode: 403, headers: nil)
                         }
-                    })
+
+                        waitUntil(timeout: .seconds(10), action: { done in
+                            _ = authenticationService.authenticateEmail(email: email, password: password) { result in
+                                switch result {
+                                case .success:
+                                    fail("result should be failure")
+                                case .failure(let error):
+                                    expect(error.errors?[0].code) == "invalid_client"
+                                    expect(error.errors?[0].detail) == "Client authentication failed due to unknown client, no client authentication included, or unsupported authentication method."
+                                }
+                                done()
+                            }
+                        })
+                    }
                 }
             }
         }
